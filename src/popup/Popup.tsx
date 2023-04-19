094f7b0c29ae8228dbba1596358dcee87e97c29e
Husain Unwalla <mr.husainunwalla@gmail.com>
2023-04-19 12:08:11 -0700

    feat- reusbale toggle component

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useChromeStorage } from '../utils/useChromeStorage';
+import Toggle from '../components/Toggle';
 
 function Popup() {
     const [toggleState, setToggleState] = useChromeStorage('youtube-interventions', false);
@@ -12,11 +13,7 @@ function Popup() {
             <div className='p-4'>
                 <h1 className='text-base mb-4 font-medium'>HabitLab 2.0</h1>
 
-                <label className="relative flex justify-between items-center group p-2 text-base">
-                    Youtube Interventions
-                    <input type="checkbox" onChange={handleToggle} checked={toggleState} className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" />
-                    <span className="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
-                </label>
+                <Toggle label="Youtube Interventions" handleToggle={handleToggle} toggleState={toggleState} />
             </div>
         </div>
     )
